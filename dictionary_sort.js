ccf1816dedd2010000ae878e2b105287900e802a
Ahyana Rizky Pratama & YONI SETIAWAN <pair+ahyanarizky.yonisetiawan@devbootcamp.com>
2016-09-27 15:43:19 +0700

    prograM works

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,5 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var arr= []
 function dictionary_sort(arr) {
   // Your code here to sort the array
+  
 }
 
 // your code here to initialize the program and take user input
+rl.setPrompt('Type a word : \n=>')
+rl.prompt()
+rl.on('line', (input) => {
+rl.prompt()
+arr.push(input)
+if(input === ""){
+  arr.pop()
+  rl.close()
+}
+}).on('close',() => {
+  console.log(`Congratulation! your dictionary has ${arr.length} words:\n `+ dictionary_sort(arr));
+});
