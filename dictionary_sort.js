0ace27e0eb3caeca5894f00138fa349f12224aaa
Ahyana Rizky Pratama & YONI SETIAWAN <pair+ahyanarizky.yonisetiawan@devbootcamp.com>
2016-09-27 17:02:54 +0700

    Dictionary Sort Done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -8,18 +8,27 @@ const rl = readline.createInterface({
 var arr= []
 function dictionary_sort(arr) {
   // Your code here to sort the array
-  
+  var values, i, j
+  for(i = 0; i<arr.length; i++){
+    values = arr[i]
+    for( j= i-1; j>=0 && arr[j] > values; j--){
+      arr[j+1]=arr[j]
+    }
+    arr[j+1] = values
+  }
+  return arr.join(",")
 }
 
 // your code here to initialize the program and take user input
 rl.setPrompt('Type a word : \n=>')
 rl.prompt()
 rl.on('line', (input) => {
-rl.prompt()
-arr.push(input)
-if(input === ""){
-  arr.pop()
-  rl.close()
+if(input==""){
+rl.close()
+} else {
+  rl.setPrompt('Type a word : (or press enter to finish)\n=>')
+  arr.push(input)
+  rl.prompt()
 }
 }).on('close',() => {
   console.log(`Congratulation! your dictionary has ${arr.length} words:\n `+ dictionary_sort(arr));
