9946b43130610a0285289fdd74fab436ec7d2ad6
Alessandro C & Mangku Widodo <pair+alessandrocls.mangkuwi@hacktiv8.com>
2016-09-27 19:04:33 +0700

    Submitted dictionary_sort.js

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,5 +1,43 @@
-function dictionary_sort(arr) {
-  // Your code here to sort the array
+var array1 = []
+var array2 = []
+
+function dictionary_sort(input) {
+  var len = input.length, value, i, j;
+
+  for (i = 0; i < len; i++){
+    value = input[i]
+    for (j = i-1; j > -1 && input[j] > value; j--){
+      input[j+1] = input[j];
+    }
+    input[j+1] = value
+  }
+  console.log(input)
 }
+
+const readline = require ('readline');
+const rl = readline.createInterface({
+ input: process.stdin,
+ output: process.stdout
+});
+
+rl.setPrompt("Type a word: ")
+rl.prompt()
+rl.on ('line', (line) => {
+
+
+if(line.trim() == ""){
+  rl.close();
+}
+else{
+  rl.setPrompt("Type another word: (Press enter to finish)");
+  array1.push(line.trim())
+
+}
+rl.prompt();
+}).on('close', () =>{
+  console.log("Your dictionary has " + array1.length + " words");
+  dictionary_sort(array1);
+  process.exit(0)
+})
 
 // your code here to initialize the program and take user input
